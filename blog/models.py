3d24ced7fb87874031e398542698bd22940c2715
MiaTothova <21tothova@gmail.com>
2025-02-08 09:02:04 +0000

    Create comment model, migrate changes. Create template/blog folder and include postlist file.

--- blog/models.py
+++ blog/models.py
@@ -9,12 +9,18 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
-)
+    )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerp = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
